72e607e5aa9d6268467299915e0a0c0ab45e7c3e
VsevolodGus <gusakseva8@gmail.com>
2022-11-20 18:35:56 +0500

    add simply sort

---
+++ TestMindBox/MindBoxTest/Sorts/SortTest.cs
@@ -0,0 +1,27 @@
+﻿using TestMindBox.Sorts;
+
+namespace MindBoxTest.Sorts;
+
+public class SortTest
+{
+    private readonly int[] _source = new int[] { 1, 4, 5, 3, 7, 8, 6, 9, 2, 0 };
+
+    private readonly IReadOnlyCollection<int> _resultAsc = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    private readonly IReadOnlyCollection<int> _resultDesc = new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+
+    [Fact]
+    private void SortByDesc()
+    {
+        var result = _source.Sorting(false);
+
+        Assert.Equal(_resultDesc, result);
+    }
+
+    [Fact]
+    private void SortByAsc()
+    {
+        var result = _source.Sorting(true);
+
+        Assert.Equal(_resultAsc, result);
+    }
+}
---
+++ TestMindBox/TestMindBox/Sorts/Sort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMindBox.Sorts;
+
+public static class Sort
+{
+    public static int[] Sorting(this int[]source, bool orderBy)
+    {
+        Func<int, int, bool> func = orderBy ? (value1, value2) => value1 > value2 
+                                            : (value1, value2) => value1 < value2;
+
+        for (int i = 0; i < source.Length; i++)
+        for (int j = i; j < source.Length; j++)
+            if (func(source[i],  source[j]))
+                    Swap(ref source[i], ref source[j]);
+
+        return source;
+    }
+
+    private static void Swap<T>(ref T value1, ref T value2)
+    {
+        var temp = value1;
+        value1 = value2;
+        value2 = temp;
+    }
+
+}
