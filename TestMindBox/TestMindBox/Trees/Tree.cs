b561e7a12a5b5b7c5aa69f9dd611188907b63176
VsevolodGus <gusakseva8@gmail.com>
2022-11-20 05:36:06 +0500

    add tree crawls

---
+++ TestMindBox/TestMindBox/Trees/Tree.cs
@@ -0,0 +1,85 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace TestMindBox.Trees;
+
+public class Node
+{
+    public Node(Node node)
+    {
+        ID = node.ID;
+        Children = (Node[])node.Children.Clone();
+    }
+    public Node()
+    {
+    }
+
+    public int ID { get; init; }
+    public Node[] Children { get; init; } = Array.Empty<Node>();
+}
+
+public static class ExtentionsNode
+{
+    public static List<int> IterateTreeInWidth(this Node n)
+    {
+        var root = new Node(n);
+        var result = new List<int>
+        {
+            root.ID
+        };
+
+        var nodes = new Queue<Node>(root.Children);
+
+        while (nodes.TryDequeue(out root))
+        {
+            result.Add(root.ID);
+
+            foreach (var item in root.Children)
+                nodes.Enqueue(item);
+        }
+
+        return result;
+    }
+
+
+    public static List<int> IterateTreeInDepthRightToLeft(this Node n)
+    {
+        var root = new Node(n);
+        var result = new List<int>();
+
+        var stack = new Stack<Node>();
+        stack.Push(root);
+
+        while (stack.TryPop(out root))
+        {
+            result.Add(root.ID);
+
+            foreach(var item in root.Children)
+                stack.Push(item);
+        }
+
+        
+        return result;
+    }
+
+
+    public static List<int> IterateTreeInDepthLeftToRight(this Node n)
+    {
+        var root = new Node(n);
+        var result = new List<int>();
+
+        var stack = new Stack<Node>();
+        stack.Push(root);
+
+        while (stack.TryPop(out root))
+        {
+            result.Add(root.ID);
+
+            foreach (var item in root.Children.Reverse())
+                stack.Push(item);
+        }
+
+
+        return result;
+    }
+
+}
