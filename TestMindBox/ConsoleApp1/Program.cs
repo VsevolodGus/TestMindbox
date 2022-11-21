0e7af745da7a02a221bf98a30f5ac796e961300d
VsevolodGus <gusakseva8@gmail.com>
2022-11-21 17:33:26 +0500

    фыв

---
+++ TestMindBox/ConsoleApp1/Program.cs
@@ -0,0 +1,6 @@
+﻿using System.Runtime.CompilerServices;
+
+int i = 1;
+object o = i;
+Console.WriteLine(object.ReferenceEquals(o, i));
+Console.WriteLine(o.Equals(i));
---
+++ TestMindBox/MindBoxTest/Test.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestMindBox;
+
+namespace MindBoxTest;
+
+public class Test
+{
+    private readonly Node _root = new Node('a')
+    {
+        Children = new Node[] 
+        {
+            new Node('b')
+            {
+                Children = new Node[]
+                {
+                    new Node('d'),
+                    new Node('e'),
+                    new Node('f')
+                },
+            },
+            new Node('c')
+            {
+                 Children = new Node[]
+                {
+                    new Node('g'),
+                    new Node('h'),
+                },
+            }
+        }
+    };
+
+    
+    private readonly BNode asd = new BNode()
+    {
+
+    };
+
+    //private 
+    //[Fact]
+    public void test()
+    {
+        var result = _root.GetAllNodes();
+        var excepted = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h' };
+        Assert.Equal(excepted, result);
+    }
+
+    //[Fact]
+    //public void tasdest()
+    //{
+    //    var result = asd.GetAllNodes();
+    //    var excepted = new char[] { asd, asd.Children[0] };
+    //    Assert.Equal(excepted, result);
+    //}
+}
