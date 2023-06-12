85237776d3b3b5be60d66dc517320526a7ad0c0e
VsevolodGus <gusakseva8@gmail.com>
2023-06-13 00:19:07 +0500

    add task from Codewars

---
+++ TestMindBox/TestMindBox/Codewars/CalculateVelueOfTwoArrays.cs
@@ -0,0 +1,12 @@
+﻿namespace TestMindBox.Codewars;
+internal class CalculateVelueOfTwoArrays
+{
+    public static double Solution(int[] firstArray, int[] secondArray)
+    {
+        var result = 0.0;
+        for(int i = 0; i< firstArray.Length; i++)  
+            result += Math.Pow(firstArray[i] - secondArray[i], 2);
+
+        return result / firstArray.Length;
+    }
+}
