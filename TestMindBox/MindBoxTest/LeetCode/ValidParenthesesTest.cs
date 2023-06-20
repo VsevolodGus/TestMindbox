792c271879c09e6fd76629f88abec04e9ee5d2e4
user <user@WIN-2A6KSVNFBG3>
2023-06-21 01:32:41 +0500

    add task from leetCode

---
+++ TestMindBox/MindBoxTest/LeetCode/ValidParenthesesTest.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestMindBox.LeetCode;
+
+namespace MindBoxTest.LeetCode;
+public class ValidParenthesesTest
+{
+    private readonly ValidParentheses worker = new();
+    
+    [Fact]
+    public void HasCloseParentheses()
+    {
+        const string s = "()()";
+        var result = worker.IsValid(s);
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void HasNoCloseParentheses()
+    {
+        const string s = "()(";
+        var result = worker.IsValid(s);
+        Assert.False(result);
+    }
+    [Fact]
+    public void HasCloseSquareBrackets()
+    {
+        const string s = "[[]]";
+        var result = worker.IsValid(s);
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void HasNoCloseSquareBrackets()
+    {
+        const string s = "[]]";
+        var result = worker.IsValid(s);
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void HasNoCloseCurlyBrackets()
+    {
+        const string s = "{{}";
+        var result = worker.IsValid(s);
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void HasCloseCurlyBrackets()
+    {
+        const string s = "{}{}";
+        var result = worker.IsValid(s);
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void HasCloseAllBrackets()
+    {
+        const string s = "{}()[]";
+        var result = worker.IsValid(s);
+        Assert.True(result);
+    }
+    
+    [Fact]
+    public void HasCloseInsideAllBrackets()
+    {
+        const string s = "{([])}";
+        var result = worker.IsValid(s);
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void HasCloseDifferentBrackets()
+    {
+        const string s = "(}";
+        var result = worker.IsValid(s);
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void HasNoCloseBrackets()
+    {
+        const string s = ")(";
+        var result = worker.IsValid(s);
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void DoubleBrackets()
+    {
+        const string s = "([}}])";
+        var result = worker.IsValid(s);
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void DoubleBrackets_Second()
+    {
+        const string s = "))";
+        var result = worker.IsValid(s);
+        Assert.False(result);
+    }
+}
---
+++ TestMindBox/TestMindBox/LeetCode/ValidParentheses.cs
@@ -0,0 +1,29 @@
+﻿namespace TestMindBox.LeetCode;
+public class ValidParentheses
+{
+    public bool IsValid(string s)
+    {
+        if (s.Length % 2 != 0)
+             return false;
+
+        var stack = new Stack<char>();
+        var openBrackets = new char[] {'(','[','{' };
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (openBrackets.Contains(s[i]))
+                stack.Push(s[i]);
+            else if (stack.TryPeek(out char bracket))
+            {
+                if( (bracket == '(' && s[i] == ')')
+                    || (bracket == '[' && s[i] == ']')
+                    || (bracket == '{' && s[i] == '}'))
+                    stack.Pop();
+                else
+                    return false;
+            }
+            else return false;
+        }
+
+        return !stack.Any();
+    }
+}
