14f6443b10834ae5e76a79b701383eabde049256
user <user@WIN-2A6KSVNFBG3>
2023-06-22 02:22:44 +0500

    add task from LeetCode

---
+++ TestMindBox/MindBoxTest/LeetCode/RemoveElementTaskTest.cs
@@ -0,0 +1,18 @@
+﻿using TestMindBox.LeetCode;
+
+namespace MindBoxTest.LeetCode;
+
+public class RemoveElementTaskTest
+{
+    private readonly RemoveElementTask _worker = new();
+    
+    [Fact]
+    public void FirstTest()
+    {
+        var nums = new int[] { 3,2,2,3};
+        const int val = 3;
+        var result =  _worker.RemoveElement(nums, val);
+        Assert.Equal(2, result);
+    }
+       
+}
---
+++ TestMindBox/TestMindBox/LeetCode/RemoveElementTask.cs
@@ -0,0 +1,23 @@
+﻿namespace TestMindBox.LeetCode;
+public class RemoveElementTask
+{
+    public int RemoveElement(int[] nums, int val)
+    {
+        var currrent = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == val)
+                continue;
+
+            nums[currrent] = nums[i];
+            currrent++;
+        }
+
+       return currrent;
+
+        //    var result = nums.OrderBy(c=> c == val).ToArray();
+        //    Array.Copy(result, nums, result.Length);
+        //    return nums.Count(c=> c != val);
+    }
+}
