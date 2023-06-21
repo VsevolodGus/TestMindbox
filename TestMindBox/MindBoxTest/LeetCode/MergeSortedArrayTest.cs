25261ef3982073080f31ed31347e21d0a9687c13
user <user@WIN-2A6KSVNFBG3>
2023-06-22 02:00:21 +0500

    add task from leetCode

---
+++ TestMindBox/MindBoxTest/LeetCode/MergeSortedArrayTest.cs
@@ -0,0 +1,21 @@
+﻿using TestMindBox.LeetCode;
+
+namespace MindBoxTest.LeetCode;
+public class MergeSortedArrayTest
+{
+    private readonly MergeSortedArray _worker = new();
+
+    [Fact]
+    public void FirstTest()
+    {
+        var nums1 = new int[] {1,2,3,0,0,0};
+        var m = 3;
+        
+        var nums2 = new int[] {2,5,6};
+        var n = 3;
+
+        _worker.Merge(nums1, m, nums2, n);
+
+        Assert.Equal(new int[] { 1, 2, 2, 3, 5, 6 }, nums1);
+    }
+}
---
+++ TestMindBox/TestMindBox/LeetCode/MergeSortedArray.cs
@@ -0,0 +1,16 @@
+﻿namespace TestMindBox.LeetCode;
+public class MergeSortedArray
+{
+    public void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        //var arr1 = nums1.Take(m).ToList();
+        //arr1.AddRange(nums2.Take(n));
+
+        //var result = arr1.OrderBy(c => c).ToArray();
+
+        //Array.Copy(result, nums1, result.Length);
+
+        Array.Copy(nums2, 0, nums1, m, n);
+        Array.Sort(nums1);
+    }
+}
