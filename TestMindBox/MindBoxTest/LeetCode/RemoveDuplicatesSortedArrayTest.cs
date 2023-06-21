42aa9f6549b2cb79f99ee9b2d3e3ae9cfeb79e13
user <user@WIN-2A6KSVNFBG3>
2023-06-22 02:57:13 +0500

    add task from LettCode

---
+++ TestMindBox/MindBoxTest/LeetCode/RemoveDuplicatesSortedArrayTest.cs
@@ -0,0 +1,25 @@
+﻿using TestMindBox.LeetCode;
+
+namespace MindBoxTest.LeetCode;
+public class RemoveDuplicatesSortedArrayTest
+{
+    private readonly RemoveDuplicatesSortedArray _worker = new();
+
+    [Fact]
+    public void FirstTest()
+    {
+        var nums = new int[] { 1,1,2};
+        var result = _worker.RemoveDuplicates(nums);
+
+        Assert.Equal(2, result);
+    }
+
+    [Fact]
+    public void SecondTest()
+    {
+        var nums = new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+        var result = _worker.RemoveDuplicates(nums);
+
+        Assert.Equal(5, result);
+    }
+}
