5f0890291e71bff152fb0e1499da00527d2492e9
user <user@WIN-2A6KSVNFBG3>
2023-06-28 02:58:55 +0500

    add task from leet code

---
+++ TestMindBox/MindBoxTest/LeetCode/RotateArrayTaskTest.cs
@@ -0,0 +1,30 @@
+﻿using TestMindBox.LeetCode;
+
+namespace MindBoxTest.LeetCode;
+public class RotateArrayTaskTest
+{
+    private readonly RotateArrayTask _worker = new();
+
+
+    [Fact]
+    public void FirstTask()
+    {
+        var nums = new[] { 1, 2, 3, 4, 5, 6, 7 };
+        const int k = 3;
+
+        _worker.Rotate(nums, k);
+
+        Assert.Equal(new int[] { 5, 6, 7, 1, 2, 3, 4 }, nums);
+    }
+
+    [Fact]
+    public void SecondTask()
+    {
+        var nums = new[] { -1, -100, 3, 99 };
+        const int k = 2;
+
+        _worker.Rotate(nums, k);
+
+        Assert.Equal(new int[] { 3, 99, -1, -100 }, nums);
+    }
+}
---
+++ TestMindBox/TestMindBox/LeetCode/RotateArrayTask.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMindBox.LeetCode;
+public class RotateArrayTask
+{
+    public void Rotate(int[] nums, int k)
+    {
+        int n = nums.Length;
+        k %= n;
+
+        Reverse(nums, 0, n - 1);
+        Reverse(nums, 0, k - 1);
+        Reverse(nums, k, n - 1);
+    }
+
+    private void Reverse(int[] nums, int left, int right) 
+    {
+        while(left < right) 
+        {
+            (nums[left], nums[right]) = (nums[right], nums[left]);
+            left++;
+            right--;
+        }
+    }
+}
