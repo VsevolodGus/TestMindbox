b49a166c18f9bd826a2b801c446f2832ff392199
user <user@WIN-2A6KSVNFBG3>
2023-06-28 01:54:55 +0500

    add task from leet code

---
+++ TestMindBox/MindBoxTest/LeetCode/MajorityElementTaskTest.cs
@@ -0,0 +1,26 @@
+﻿using TestMindBox.LeetCode;
+
+namespace MindBoxTest.LeetCode;
+public class MajorityElementTaskTest
+{
+    private readonly MajorityElementTask _worker = new();
+    [Fact]
+    public void FirstTest()
+    {
+        var nums = new int[] { 3, 2, 3 };
+
+        var result = _worker.MajorityElement(nums);
+
+        Assert.Equal(3, result);
+    }
+
+    [Fact]
+    public void SecondTest()
+    {
+        var nums = new int[] { 2, 2, 1, 1, 1, 2, 2 };
+
+        var result = _worker.MajorityElement(nums);
+
+        Assert.Equal(2, result);
+    }
+}
---
+++ TestMindBox/TestMindBox/LeetCode/MajorityElementTask.cs
@@ -0,0 +1,19 @@
+﻿namespace TestMindBox.LeetCode;
+public class MajorityElementTask
+{
+    public int MajorityElement(int[] nums)
+    {
+        var count = 0;
+        var candidateValue = 0;
+
+        foreach (var item in nums)
+        {
+            if(count == 0) 
+                candidateValue = item;
+
+            count += (candidateValue == item) ? 1 :-1;
+        }
+
+        return candidateValue;
+    }
+}
