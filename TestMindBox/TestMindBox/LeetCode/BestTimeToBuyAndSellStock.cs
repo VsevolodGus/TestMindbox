af1e4c5919d0ba083facc23602fffb984ce58e98
user <user@WIN-2A6KSVNFBG3>
2023-06-29 02:20:52 +0500

    solved the problem again

--- TestMindBox/TestMindBox/LeetCode/BestTimeToBuyAndSellStock.cs
+++ TestMindBox/TestMindBox/LeetCode/BestTimeToBuyAndSellStock.cs
@@ -4,21 +4,21 @@ public class BestTimeToBuyAndSellStock
     //https://leetcode.com/problems/best-time-to-buy-and-sell-stock/?envType=study-plan-v2&envId=top-interview-150
     public int MaxProfit(int[] prices)
     {
-        if(prices.Length <= 1)
+        if(prices.Length <= 1) 
             return 0;
 
-        var max = 0;
+        var maxResult = 0;
         var min = prices[0];
 
-        for (int i = 1; i < prices.Length; i++)
+        for (int i = 1; i < prices.Length; i++) 
         {
-            if (min > prices[i])
+            if (prices[i] < min)
                 min = prices[i];
-            else if(prices[i] - min > max)
-                max = prices[i] - min;
+            else if(prices[i] - min > maxResult)
+                maxResult = prices[i] - min;
         }
 
-        return max;
+        return maxResult;
     }
 
     //TODO
