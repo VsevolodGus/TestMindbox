f8af71b97f5fe051aa846deada346e0c68cf4ac6
user <user@WIN-2A6KSVNFBG3>
2023-06-29 02:46:11 +0500

    add task from LeetCode

---
+++ TestMindBox/MindBoxTest/LeetCode/LengthLastWordTest.cs
@@ -0,0 +1,38 @@
+﻿using TestMindBox.LeetCode;
+
+namespace MindBoxTest.LeetCode;
+public class LengthLastWordTest
+{
+    private readonly LengthLastWordTask _worker = new();
+
+    [Fact]
+    public void SimpleTest()
+    {
+        const string s = "Hello World";
+
+        var result = _worker.LengthOfLastWord(s);
+
+        Assert.Equal(5, result);
+    }
+
+    [Fact]
+    public void WithSeveralWordsTest()
+    {
+        const string s = "luffy is still joyboy";
+
+        var result = _worker.LengthOfLastWord(s);
+
+        Assert.Equal(6, result);
+    }
+
+    [Fact]
+    public void WithSpaceInEndTest()
+    {
+        const string s = "   fly me   to   the moon  ";
+
+        var result = _worker.LengthOfLastWord(s);
+
+        Assert.Equal(4, result);
+    }
+
+}
---
+++ TestMindBox/TestMindBox/LeetCode/LengthLastWordTask.cs
@@ -0,0 +1,52 @@
+﻿namespace TestMindBox.LeetCode;
+public class LengthLastWordTask
+{
+    public int LengthOfLastWord(string s)
+    {
+        #region Simple solutin
+        //var words = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        //return words.Last().Length;
+        #endregion
+
+
+        var result = 0;
+        #region My optimized solution
+        //var startWord = false;
+        //for (int i = 0; i < s.Length; i++)
+        //{
+        //    if (!startWord)
+        //    {
+        //        if (s[s.Length - 1 - i] != ' ')
+        //        {
+        //            result++;
+        //            startWord = true;
+        //        }
+
+        //        continue;
+        //    }
+
+        //    if (s[s.Length - 1 - i] == ' ')
+        //        break;
+
+        //    result++;
+        //}
+        #endregion
+
+        #region Optimized solution
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[s.Length - 1 - i] == ' ')
+            {
+                if (result == 0)
+                    continue;
+                else
+                    break;
+            }
+
+            result++;
+        }
+        #endregion
+
+        return result;
+    }
+}
