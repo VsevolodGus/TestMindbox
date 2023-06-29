44eb122168e0f87644a935eba9c08dcba41fce57
user <user@WIN-2A6KSVNFBG3>
2023-06-30 03:34:37 +0500

    add task from LeetCode

---
+++ TestMindBox/MindBoxTest/LeetCode/FindIndexFirstOccurrenceInStringTest.cs
@@ -0,0 +1,40 @@
+﻿using TestMindBox.LeetCode;
+
+namespace MindBoxTest.LeetCode;
+public class FindIndexFirstOccurrenceInStringTest
+{
+    private readonly FindIndexFirstOccurrenceInString _worker = new();
+
+    [Fact]
+    public void HasNeedle()
+    {
+        const string haystack = "sadbutsad";
+        const string needle = "sad";
+
+        var result = _worker.StrStr(haystack, needle);
+        
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void NoHasNeedle()
+    {
+        const string haystack = "leetcode";
+        const string needle = "leeto";
+
+        var result = _worker.StrStr(haystack, needle);
+
+        Assert.Equal(-1, result);
+    }
+
+    [Fact]
+    public void HasOneSymbol()
+    {
+        const string haystack = "a";
+        const string needle = "a";
+
+        var result = _worker.StrStr(haystack, needle);
+
+        Assert.Equal(0, result);
+    }
+}
---
+++ TestMindBox/TestMindBox/LeetCode/FindIndexFirstOccurrenceInString.cs
@@ -0,0 +1,41 @@
+﻿namespace TestMindBox.LeetCode;
+
+/// <summary>
+/// https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/?envType=study-plan-v2
+/// </summary>&envId=top-interview-150
+public class FindIndexFirstOccurrenceInString
+{
+    public int StrStr(string haystack, string needle)
+    {
+        #region MySolution
+        //var result = -1;
+        //for(int i = 0; i< haystack.Length - needle.Length + 1; i++)
+        //{
+        //    var sub =  haystack.Substring(i, needle.Length);
+        //    if (!needle.Equals(sub))
+        //        continue;
+
+        //    result = i;
+        //    break;
+        //}
+
+        //return result;
+        #endregion
+
+        var count = -1;
+
+        for (int i = 0; i < haystack.Length; i++)
+        {
+            count++;
+            if (haystack[i] == needle[count] && count == needle.Length - 1)
+                return i - count;
+            else if (haystack[i] != needle[count])
+            { 
+                i -= count;
+                count = -1;
+            }
+        }
+
+        return count;
+    }
+}
