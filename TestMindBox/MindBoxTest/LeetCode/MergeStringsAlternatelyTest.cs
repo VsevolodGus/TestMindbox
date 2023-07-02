6ded307205ff087272ba6781eab75745c3b0ed47
user <user@WIN-2A6KSVNFBG3>
2023-07-02 18:52:00 +0500

    add tast from LeetCode

---
+++ TestMindBox/MindBoxTest/LeetCode/MergeStringsAlternatelyTest.cs
@@ -0,0 +1,32 @@
+﻿using TestMindBox.LeetCode;
+
+namespace MindBoxTest.LeetCode;
+public class MergeStringsAlternatelyTest
+{
+    private readonly MergeStringsAlternately _worker = new();
+
+
+    [Fact]
+    public void LenghtWordsEqueal()
+    {
+        var result = _worker.MergeAlternately("abc", "pqr");
+
+        Assert.Equal("apbqcr", result);
+    }
+
+    [Fact]
+    public void SecondMoreLenght()
+    {
+        var result = _worker.MergeAlternately("ab", "pqrs");
+
+        Assert.Equal("apbqrs", result);
+    }
+
+    [Fact]
+    public void FirstMoreLenght()
+    {
+        var result = _worker.MergeAlternately("abcd", "pq");
+
+        Assert.Equal("apbqcd", result);
+    }
+}
---
+++ TestMindBox/TestMindBox/LeetCode/MergeStringsAlternately.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+
+namespace TestMindBox.LeetCode;
+
+/// <summary>
+/// https://leetcode.com/studyplan/leetcode-75/
+/// </summary>
+public class MergeStringsAlternately
+{
+
+    public string MergeAlternately(string word1, string word2)
+    {
+        #region Мое решение, через массив
+        //var result = new char[word1.Length + word2.Length];
+
+        //var minLenght = Math.Min(word1.Length, word2.Length);
+
+        //for (int i = 0; i < minLenght; i++) 
+        //    result[i*2] = word1[i];
+
+        //for (int i = 0; i < minLenght; i++)
+        //    result[(i*2)+1] = word2[i];
+
+        //if(word1.Length == word2.Length)
+        //    return new string(result.ToArray());
+
+
+        //var str = word1.Length == minLenght ? word2 : word1;
+
+        //for (int i = 0; i < str.Length - minLenght; i++)
+        //    result[minLenght * 2 + i] = str[minLenght + i];
+
+        //return new string(result.ToArray());
+        #endregion
+
+        var result = new StringBuilder(word1.Length + word2.Length);
+        int indexFirstWord = 0;
+        int indexSecondWord = 0;
+
+        while (indexFirstWord < word1.Length && indexSecondWord < word2.Length)
+        {
+            result.Append(word1[indexFirstWord])
+                  .Append(word2[indexSecondWord]);
+
+            indexFirstWord++;
+            indexSecondWord++;
+        }
+
+        while (indexFirstWord < word1.Length)
+        {
+            result.Append(word1[indexFirstWord]);
+            indexFirstWord++;
+        }
+
+        while (indexSecondWord < word2.Length)
+        {
+            result.Append(word2[indexSecondWord]);
+            indexSecondWord++;
+        }
+
+        return result.ToString();
+    }
+}
