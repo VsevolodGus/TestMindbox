c6002ba8756cdc82937937a8439c428c52c0b98d
user <user@WIN-2A6KSVNFBG3>
2023-07-05 03:43:54 +0500

    add task from leetCode

---
+++ TestMindBox/TestMindBox/LeetCode/KidsWithGreatestNumberCandies.cs
@@ -0,0 +1,21 @@
+﻿namespace TestMindBox.LeetCode;
+
+public class KidsWithGreatestNumberCandies
+{
+    public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+    {
+        var result = new bool[candies.Length];
+
+        for(int i = 0; i< candies.Length; i++) 
+        {
+            candies[i] += extraCandies;
+
+            var max = candies.Max();
+            result[i] = candies[i] == max;
+
+            candies[i] -= extraCandies;
+        }
+
+        return result;
+    }
+}
