f6a3bd9fcd71c2c5a88257a12e842c34605b475f
user <user@WIN-2A6KSVNFBG3>
2023-07-05 19:42:32 +0500

    add task from LetCode

---
+++ TestMindBox/TestMindBox/LeetCode/MoveZeroesTask.cs
@@ -0,0 +1,24 @@
+﻿namespace TestMindBox.LeetCode;
+/// <summary>
+/// https://leetcode.com/problems/move-zeroes/description/
+/// </summary>
+public class MoveZeroesTask
+{
+    /// <summary>
+    /// Решение нужно прописывать руками чтобы понять
+    /// </summary>
+    /// <param name="nums"></param>
+    public void MoveZeroes(int[] nums)
+    {
+        var firstPosZero = 0;
+
+        for(int i = 0; i< nums.Length; i++) 
+        {
+            if (nums[i] == 0)
+                continue;
+
+            (nums[i], nums[firstPosZero]) = (nums[firstPosZero], nums[i]);
+            firstPosZero++;
+        }
+    }
+}
