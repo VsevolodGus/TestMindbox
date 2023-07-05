b2c5c20f682cc349b0285102b94e8b46b2b5a37e
user <user@WIN-2A6KSVNFBG3>
2023-07-05 19:56:11 +0500

    add task from LeetCode

---
+++ TestMindBox/MindBoxTest/LeetCode/SubsequenceTaskTest.cs
@@ -0,0 +1,52 @@
+﻿using TestMindBox.LeetCode;
+
+namespace MindBoxTest.LeetCode;
+public class SubsequenceTaskTest
+{
+    private readonly SubsequenceTask _worker = new();
+
+    [Fact]
+    public void IsSubsequence()
+    {
+        const string s = "abc";
+        const string t = "ahbgdc";
+
+        var result = _worker.IsSubsequence(s, t);
+        
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void IsNotSubsequence()
+    {
+        const string s = "axc";
+        const string t = "ahbgdc";
+
+        var result = _worker.IsSubsequence(s, t);
+
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void S_IsEmpty()
+    {
+        const string s = "";
+        const string t = "ahbgdc";
+
+        var result = _worker.IsSubsequence(s, t);
+
+        Assert.True(result);
+    }
+
+
+    [Fact]
+    public void IfSEndInMiddeleT()
+    {
+        const string s = "b";
+        const string t = "abc";
+
+        var result = _worker.IsSubsequence(s, t);
+
+        Assert.True(result);
+    }
+}
---
+++ TestMindBox/TestMindBox/LeetCode/SubsequenceTask.cs
@@ -0,0 +1,26 @@
+﻿namespace TestMindBox.LeetCode;
+
+/// <summary>
+/// https://leetcode.com/problems/is-subsequence/?envType=study-plan-v2&envId=leetcode-75
+/// </summary>
+public class SubsequenceTask
+{
+    public bool IsSubsequence(string s, string t)
+    {
+        if (string.IsNullOrEmpty(s))
+            return true;
+
+        int indexForS = 0;
+
+        for (int i = 0; i < t.Length; i++) 
+        {
+            if(indexForS == s.Length)
+                break;
+
+            if (s[indexForS] == t[i])
+                indexForS++;
+        }
+
+        return indexForS == s.Length;
+    }
+}
