a9725b4e974251adfd968397db2d9ed681ffe296
user <user@WIN-2A6KSVNFBG3>
2023-07-09 20:36:49 +0500

    add task from LeetCode

---
+++ TestMindBox/MindBoxTest/LeetCode/MaximumAverageSubarrayTaskTest.cs
@@ -0,0 +1,18 @@
+﻿using TestMindBox.LeetCode;
+
+namespace MindBoxTest.LeetCode;
+public sealed class MaximumAverageSubarrayTaskTest
+{
+    private readonly MaximumAverageSubarrayTask _worker = new();
+
+    [Theory]
+    [InlineData(new int[] { 1, 12, -5, -6, 50, 3 }, 4, 12.75000)]
+    [InlineData(new int[] { 5 }, 1, 5.00000)]
+    [InlineData(new int[] { 0, 1, 1, 3, 3 }, 4, 2.00000)]
+    public void FirstTest(int[] nums, int k, double exceptedResult)
+    {
+        var result = _worker.FindMaxAverage(nums, k);
+
+        Assert.Equal(exceptedResult, result);
+    }
+}
---
+++ TestMindBox/TestMindBox/LeetCode/MaximumAverageSubarrayTask.cs
@@ -0,0 +1,27 @@
+﻿namespace TestMindBox.LeetCode;
+/// <summary>
+/// https://leetcode.com/problems/maximum-average-subarray-i/?envType=study-plan-v2&envId=leetcode-75
+/// </summary>
+public class MaximumAverageSubarrayTask
+{
+    public double FindMaxAverage(int[] nums, int k)
+    {
+        double result = double.MinValue;
+        double sumKElements = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            sumKElements += nums[i];
+
+            if (i < k - 1)
+                continue;
+
+            var avgValue = sumKElements / k;
+            if (avgValue > result)
+                result = avgValue;
+
+            sumKElements -= nums[i + 1 -k];
+        }
+
+        return result;
+    }
+}
