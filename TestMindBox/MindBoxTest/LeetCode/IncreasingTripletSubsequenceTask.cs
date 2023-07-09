6b2acd1068bcd3133b975eea02173b287405c4ab
user <user@WIN-2A6KSVNFBG3>
2023-07-09 20:53:30 +0500

    add tasks from LeetCode

---
+++ TestMindBox/MindBoxTest/LeetCode/IncreasingTripletSubsequenceTask.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestMindBox.LeetCode;
+
+namespace MindBoxTest.LeetCode;
+public sealed class IncreasingTripletSubsequenceTask
+{
+    private readonly IncreasingTripletSubsequence _worker = new();
+    [Theory]
+    [InlineData(new int[] { 1, 2, 3, 4, 5 })]
+    [InlineData(new int[] { 2, 3, 4, 5, 6 })]
+    [InlineData(new int[] { 10, 11, 12})]
+    public void SimpleTrue(int[] nums)
+    {
+        var result = _worker.IncreasingTriplet(nums);
+
+        Assert.True(result);
+    }
+
+    [Theory]
+    [InlineData(new int[] { 5, 4, 3, 2, 1 })]
+    [InlineData(new int[] { 8, 7, 6, 5, 4, 3 })]
+    [InlineData(new int[] { 12, 11, 10 })]
+    public void SimpleFalse(int[] nums)
+    {
+        var result = _worker.IncreasingTriplet(nums);
+
+        Assert.False(result);
+    }
+
+
+    [Theory]
+    [InlineData(new int[] { 2, 1, 5, 0, 4, 6 })]
+    public void HasTripleButMixedArray_True(int[] nums)
+    {
+        var result = _worker.IncreasingTriplet(nums);
+
+        Assert.True(result);
+    }
+
+    [Theory]
+    [InlineData(new int[] { 20, 100, 10, 12, 5, 13 })]
+    public void MixedTriple_True(int[] nums)
+    {
+        var result = _worker.IncreasingTriplet(nums);
+
+        Assert.True(result);
+    }
+    
+}
---
+++ TestMindBox/TestMindBox/LeetCode/IncreasingTripletSubsequence.cs
@@ -0,0 +1,28 @@
+﻿namespace TestMindBox.LeetCode;
+
+/// <summary>
+/// https://leetcode.com/problems/increasing-triplet-subsequence/description/
+/// </summary>
+public class IncreasingTripletSubsequence
+{
+    public bool IncreasingTriplet(int[] nums)
+    {
+        if (nums.Length < 3)
+            return false;
+
+        int firstSmallestValue = int.MaxValue;
+        int secondSmallestValue = int.MaxValue;
+
+        for (int i = 0; i < nums.Length; i++) 
+        {
+            if (nums[i] <= firstSmallestValue)
+                firstSmallestValue = nums[i];
+            else if (nums[i] <= secondSmallestValue)
+                secondSmallestValue = nums[i];
+            else
+                return true;
+        }
+
+        return false;
+    }
+}
