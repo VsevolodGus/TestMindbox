524356f91d2a929e929c2ba1bf4ba09fbf96a1c6
user <user@WIN-2A6KSVNFBG3>
2023-07-10 23:28:22 +0500

    add task from LeetCode

---
+++ TestMindBox/MindBoxTest/LeetCode/CountingBitsTask.cs
@@ -0,0 +1,20 @@
+﻿namespace MindBoxTest.LeetCode;
+
+/// <summary>
+/// https://leetcode.com/problems/counting-bits/description/
+/// </summary>
+public sealed class CountingBitsTask
+{
+    public int[] CountBits(int n)
+    {
+        var result = new int[n + 1];
+
+        for (int i = 0; i < n + 1; i++)
+        {
+            var binary = Convert.ToString(i, 2);
+            result[i] = binary.Count(c => c == '1');
+        }
+
+        return result;
+    }
+}
---
+++ TestMindBox/TestMindBox/LeetCode/CountingBitsTask.cs
@@ -0,0 +1,20 @@
+﻿namespace TestMindBox.LeetCode;
+
+/// <summary>
+/// https://leetcode.com/problems/counting-bits/?envType=study-plan-v2&envId=leetcode-75
+/// </summary>
+public class CountingBitsTask
+{
+    public int[] CountBits(int n)
+    {
+        var result = new int[n+1];
+
+        for(int i = 0; i < n+1; i++) 
+        {
+            var binary = Convert.ToString(i, 2);
+            result[i] = binary.Count(c=> c == '1');
+        }
+
+        return result;
+    }
+}
