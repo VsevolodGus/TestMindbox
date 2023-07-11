175cac90d9902c6e311ffa58e764692c674baf49
user <user@WIN-2A6KSVNFBG3>
2023-07-12 02:16:20 +0500

    add tasks from LeetCode

---
+++ TestMindBox/MindBoxTest/LeetCode/DecodeStringTaskTest.cs
@@ -0,0 +1,41 @@
+﻿using TestMindBox.LeetCode;
+
+namespace MindBoxTest.LeetCode;
+public class DecodeStringTaskTest
+{
+    private readonly DecodeStringTask _worker = new();
+
+    [Theory]
+    [InlineData("asd","asd")]
+    [InlineData("qwe", "qwe")]
+    [InlineData("zxc","zxc")]
+    public void SimpleTest(string input, string exceptedResult)
+    {
+        var result = _worker.DecodeString(input);
+
+        Assert.Equal(exceptedResult, result);
+    }
+
+    [Theory]
+    [InlineData("2[a]", "aa")]
+    [InlineData("3[s]", "sss")]
+    [InlineData("1[z]", "z")]
+    public void WithRepeatElementsTest(string input, string exceptedResult)
+    {
+        var result = _worker.DecodeString(input);
+
+        Assert.Equal(exceptedResult, result);
+    }
+
+
+    [Theory]
+    [InlineData("2[a2[q]]", "aqqaqq")]
+    [InlineData("3[s3[z]]", "szzzszzzszzz")]
+    [InlineData("1[z1[q]", "zq")]
+    public void WithNestedRepeats(string input, string exceptedResult)
+    {
+        var result = _worker.DecodeString(input);
+
+        Assert.Equal(exceptedResult, result);
+    }
+}
---
+++ TestMindBox/TestMindBox/LeetCode/DecodeStringTask.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+namespace TestMindBox.LeetCode;
+
+/// <summary>
+/// https://leetcode.com/problems/decode-string/?envType=study-plan-v2&envId=leetcode-75
+/// </summary>
+public class DecodeStringTask
+{
+    public string DecodeString(string s)
+    {
+        var result = new StringBuilder();
+
+        #region Решение которое подсмотрел
+        //var stack = new Stack<(int Start, int CountRepeat)>(s.Count(c=> c == '['));
+        //var coutRepeats = 0;
+
+        //foreach (var symbol in s)
+        //{
+        //    if (char.IsDigit(symbol))
+        //        coutRepeats = coutRepeats * 10 + symbol - '0';
+        //    else if (symbol == '[')
+        //    {
+        //        stack.Push((result.Length, coutRepeats));
+        //        coutRepeats = 0;
+        //    }
+        //    else if (symbol == ']')
+        //    {
+        //        (var start, var currentCountRepeat) = stack.Pop();
+
+        //        var repeatStirng = result.ToString().Substring(start);
+
+        //        result.Append(string.Concat(Enumerable.Repeat(repeatStirng, currentCountRepeat - 1)));
+        //    }
+        //    else
+        //        result.Append(symbol);
+
+        //}
+        #endregion
+
+        return result.ToString();
+    }
+}
