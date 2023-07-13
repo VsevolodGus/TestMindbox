979919dd9b4785bb7a625f96bd3f994d08939e49
user <user@WIN-2A6KSVNFBG3>
2023-07-14 04:23:47 +0500

    add task from LeetCode

---
+++ TestMindBox/MindBoxTest/LeetCode/AsteroidCollisionTaskTest.cs
@@ -0,0 +1,50 @@
+﻿using TestMindBox.LeetCode;
+
+namespace MindBoxTest.LeetCode;
+public class AsteroidCollisionTaskTest
+{
+    private readonly AsteroidCollisionTask _worker = new();
+
+    [Fact]
+    public void FirstTest()
+    {
+        var asteroids = new int[] { 5, 10, -5 };
+
+        var result = _worker.AsteroidCollision(asteroids);
+
+        Assert.Equal(new int[] { 5, 10 }, result);
+    }
+
+    [Theory]
+    [InlineData(new int[] { 8, -8})]
+    [InlineData(new int[] { 1, -1, 1, -1})]
+    [InlineData(new int[] { 2, -2})]
+    public void ResultEmpty(int[] asteroids)
+    {
+        var result = _worker.AsteroidCollision(asteroids);
+
+        Assert.Equal(Array.Empty<int>(), result);
+    }
+
+    [Theory]
+    [InlineData(new int[] { 8, -2 }, new int[] { 8 })]
+    [InlineData(new int[] { 3, -1 }, new int[] { 3 })]
+    [InlineData(new int[] { 3, -1, -1 }, new int[] { 3 })]
+    public void SimpleOnlyRiht(int[] asteroids, int[] exceptedResult)
+    {
+        var result = _worker.AsteroidCollision(asteroids);
+
+        Assert.Equal(exceptedResult, result);
+    }
+
+    [Theory]
+    [InlineData(new int[] { 2, -8 }, new int[] { -8})]
+    [InlineData(new int[] { 1, -3 }, new int[] { -3 })]
+    [InlineData(new int[] { 1, 1, -3 }, new int[] { -3 })]
+    public void SimpleOnlyWithLeft(int[] asteroids, int[] exceptedResult)
+    {
+        var result = _worker.AsteroidCollision(asteroids);
+
+        Assert.Equal(exceptedResult, result);
+    }
+}
---
+++ TestMindBox/TestMindBox/LeetCode/AsteroidCollisionTask.cs
@@ -0,0 +1,45 @@
+﻿namespace TestMindBox.LeetCode;
+
+/// <summary>
+/// https://leetcode.com/problems/asteroid-collision/?envType=study-plan-v2&envId=leetcode-75
+/// </summary>
+public class AsteroidCollisionTask
+{
+    public int[] AsteroidCollision(int[] asteroids)
+    {
+        #region Мое решение
+        var result = new List<int>();
+        var stack = new Stack<int>();
+
+        for (var i = 0; i < asteroids.Length; i++)
+        {
+            var asteroid = asteroids[i];
+            if (!stack.Any() && asteroid < 0)
+                result.Add(asteroid);
+            else if (asteroid < 0)
+            {
+                while (asteroid != 0 && stack.TryPop(out int lastAsteroid))
+                {
+                    if (Math.Abs(asteroid) < lastAsteroid)
+                    {
+                        stack.Push(lastAsteroid);
+                        asteroid = 0;
+                    }
+                    else if (Math.Abs(asteroid) == lastAsteroid)
+                        asteroid = 0;
+
+                }
+
+                if (Math.Abs(asteroid) > 0)
+                    result.Add(asteroid);
+            }
+            else
+                stack.Push(asteroid);
+        }
+
+        result.AddRange(stack.Reverse());
+
+        return result.ToArray();
+        #endregion
+    }
+}
