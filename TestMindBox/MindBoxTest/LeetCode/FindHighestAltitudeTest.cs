92a0090f8609844c00e8a5855e30018d360bf212
user <user@WIN-2A6KSVNFBG3>
2023-07-14 02:01:47 +0500

    add task from LeetCode

---
+++ TestMindBox/MindBoxTest/LeetCode/FindHighestAltitudeTest.cs
@@ -0,0 +1,28 @@
+﻿using TestMindBox.LeetCode;
+
+namespace MindBoxTest.LeetCode;
+
+public class FindHighestAltitudeTest
+{
+    private readonly FindHighestAltitude _worker = new();
+    
+    [Fact]
+    public void FirstTest()
+    {
+        var gain = new int[] { -5, 1, 5, 0, -7 };
+
+        var result = _worker.LargestAltitude(gain);
+
+        Assert.Equal(1, result);
+    }
+
+    [Fact]
+    public void SecondTest()
+    {
+        var gain = new int[] { -4, -3, -2, -1, 4, 3, 2 };
+
+        var result = _worker.LargestAltitude(gain);
+
+        Assert.Equal(0, result);
+    }
+}
---
+++ TestMindBox/TestMindBox/LeetCode/FindHighestAltitude.cs
@@ -0,0 +1,19 @@
+﻿namespace TestMindBox.LeetCode;
+public class FindHighestAltitude
+{
+    public int LargestAltitude(int[] gain)
+    {
+        var result = 0;
+        var currentAltitude = 0;
+
+        foreach (var a in gain) 
+        {
+            currentAltitude += a;
+            if(result < currentAltitude) 
+                result = currentAltitude;
+        }
+
+        return result;
+    }
+
+}
