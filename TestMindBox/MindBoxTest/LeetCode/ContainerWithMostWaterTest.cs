97f3a5949759a7d2697df7fa7158de20f01156f7
user <user@WIN-2A6KSVNFBG3>
2023-07-14 23:27:43 +0500

    add tasks from LeetCode

---
+++ TestMindBox/MindBoxTest/LeetCode/ContainerWithMostWaterTest.cs
@@ -0,0 +1,30 @@
+﻿using TestMindBox.LeetCode;
+
+namespace MindBoxTest.LeetCode;
+
+
+public class ContainerWithMostWaterTest
+{
+    private readonly ContainerWithMostWater _worker = new();
+
+    [Theory]
+    [InlineData(new int[] { 1,1 }, 1)]
+    [InlineData(new int[] { 1,2 }, 1)]
+    [InlineData(new int[] { 3,2 }, 2)]
+    public void SimpleTest(int[] heights, int exceptedResult)
+    {
+        var result = _worker.MaxArea(heights);
+
+        Assert.Equal(exceptedResult, result);
+    }
+
+
+    [Theory]
+    [InlineData(new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 }, 49)]
+    public void Test(int[] heights, int exceptedResult)
+    {
+        var result = _worker.MaxArea(heights);
+
+        Assert.Equal(exceptedResult, result);
+    }
+}
---
+++ TestMindBox/TestMindBox/LeetCode/ContainerWithMostWater.cs
@@ -0,0 +1,34 @@
+﻿namespace TestMindBox.LeetCode;
+
+/// <summary>
+/// https://leetcode.com/problems/container-with-most-water/description/?envType=study-plan-v2&envId=leetcode-75
+/// </summary>
+public class ContainerWithMostWater
+{
+    public int MaxArea(int[] height)
+    {
+        var result = int.MinValue;
+        int leftIndex = 0;
+        int rightIndex = height.Length - 1;
+
+        while(leftIndex < rightIndex) 
+        {
+            int currentArea;
+            if (height[leftIndex] < height[rightIndex])
+            {
+                currentArea = height[leftIndex] * (rightIndex - leftIndex);
+                leftIndex++;
+            }
+            else
+            {
+                currentArea = height[rightIndex] * (rightIndex - leftIndex);
+                rightIndex--;
+            }
+
+            if(currentArea > result)
+                result = currentArea;
+        }
+
+        return result;
+    }
+}
