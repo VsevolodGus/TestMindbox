a52a4ccd51a298ca363134aaa790feee87861492
user <user@WIN-2A6KSVNFBG3>
2023-07-14 23:57:50 +0500

    add task from LeetCode

---
+++ TestMindBox/MindBoxTest/LeetCode/ProductOfArrayExceptSelfTest.cs
@@ -0,0 +1,38 @@
+﻿using TestMindBox.LeetCode;
+
+namespace MindBoxTest.LeetCode;
+public class ProductOfArrayExceptSelfTest
+{
+    private readonly ProductOfArrayExceptSelf _worker = new();
+
+    [Theory]
+    [InlineData(new int[] { 1, 0, 0, 1 })]
+    [InlineData(new int[] { 1, 0, 1, 1, 22, 345, 0 })]
+    [InlineData(new int[] { 1, 0, 1, 0, 22, 345, 0 })]
+    public void IfContainsMoreTwoZero(int[] nums)
+    {
+        var result = _worker.ProductExceptSelf(nums);
+
+        Assert.Equal(Enumerable.Repeat(0, nums.Length), result);
+    }
+
+    [Theory]
+    [InlineData(new int[] { 1, 0, 3, 4 }, new int[] { 0, 12, 0, 0 })]
+    [InlineData(new int[] { 1, 2, 3, 0 }, new int[] { 0, 0, 0, 6 })]
+    public void HasOneZero(int[] nums, int[] exceptedResult)
+    {
+        var result = _worker.ProductExceptSelf(nums);
+
+        Assert.Equal(exceptedResult, result);
+    }
+
+
+    [Theory]
+    [InlineData(new int[] { 1, 2, 3, 4 }, new int[] { 24, 12, 8, 6 })]
+    public void HasNotZero(int[] nums, int[] exceptedResult)
+    {
+        var result = _worker.ProductExceptSelf(nums);
+
+        Assert.Equal(exceptedResult, result);
+    }
+}
---
+++ TestMindBox/TestMindBox/LeetCode/ProductOfArrayExceptSelf.cs
@@ -0,0 +1,44 @@
+﻿namespace TestMindBox.LeetCode;
+
+/// <summary>
+/// https://leetcode.com/problems/product-of-array-except-self/description/?envType=study-plan-v2&envId=leetcode-75
+/// </summary>
+public class ProductOfArrayExceptSelf
+{
+    /// <summary>
+    /// Рассматриваю три случая
+    /// 1) если больше 1 нуля, то все значения 0
+    /// 2) если есть 
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int[] ProductExceptSelf(int[] nums)
+    {
+        var result = new int[nums.Length];
+
+        if (nums.Count(c => c == 0) == 1)
+        {
+            int indexZero = 0;
+            int a = 1;
+            for (int i = 0; i < nums.Length; i++)
+                if (nums[i] != 0)
+                    a *= nums[i];
+                else
+                    indexZero = i
+
+            result[indexZero] = a;
+        }
+        else if(nums.All(c => c != 0))
+        {
+            int a = 1;
+            foreach (int i in nums)
+                    a *= i;
+
+            for (int i = 0; i < nums.Length; i++)
+                result[i] = a / nums[i];
+        }
+
+        return result;
+        
+    }
+}
