d4c5788615d5fa6d0cc0083e2650776217faecdf
user <user@WIN-2A6KSVNFBG3>
2023-07-15 15:29:28 +0500

    add task from LeetCode

---
+++ TestMindBox/MindBoxTest/LeetCode/KthLargestElementInArrayTest.cs
@@ -0,0 +1,18 @@
+﻿using TestMindBox.LeetCode;
+
+namespace MindBoxTest.LeetCode;
+public class KthLargestElementInArrayTest
+{
+    private readonly KthLargestElementInArray _worker = new();
+
+    [Theory]
+    [InlineData(new int[] { 3, 2, 1, 5, 6, 4 }, 2, 5)]
+    [InlineData(new int[] { 3, 2, 3, 1, 2, 4, 5, 5, 6 }, 4, 4)]
+    public void Test(int[] nums,int k, int exepcedResult)
+    {
+        var result = _worker.FindKthLargest(nums, k);
+
+        Assert.Equal(exepcedResult, result);
+    }
+
+}
---
+++ TestMindBox/TestMindBox/LeetCode/KthLargestElementInArray.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Data;
+
+namespace TestMindBox.LeetCode;
+/// <summary>
+/// https://leetcode.com/problems/kth-largest-element-in-an-array/?envType=study-plan-v2&envId=leetcode-75
+/// </summary>
+public class KthLargestElementInArray
+{
+    public int FindKthLargest(int[] nums, int k)
+    {
+        var queue = new PriorityQueue<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            queue.Enqueue(nums[i], nums[i]);
+
+            if(queue.Count > k)
+                queue.Dequeue();
+        }
+
+        return queue.Dequeue();
+    }
+}
