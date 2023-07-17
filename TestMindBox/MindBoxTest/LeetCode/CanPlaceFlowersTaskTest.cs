63c64211d374c9be84bc33f9a1e8ae80a725e385
user <user@WIN-2A6KSVNFBG3>
2023-07-18 02:23:39 +0500

    add task from LeetCode

---
+++ TestMindBox/MindBoxTest/LeetCode/CanPlaceFlowersTaskTest.cs
@@ -0,0 +1,29 @@
+﻿using TestMindBox.LeetCode;
+
+namespace MindBoxTest.LeetCode;
+public class CanPlaceFlowersTaskTest
+{
+    private readonly CanPlaceFlowersTask _worker = new();
+
+    [Theory]
+    [InlineData(new int[] { 1, 0, 0, 0, 1 }, 1)]
+    [InlineData(new int[] { 1, 0, 0, 0, 0, 0, 1 }, 2)]
+    public void CanViolating(int[] flowers, int n)
+    {
+        var result = _worker.CanPlaceFlowers(flowers, n);
+
+        Assert.True(result);
+    }
+
+
+    [Theory]
+    [InlineData(new int[] { 1, 0 }, 1)]
+    [InlineData(new int[] { 1, 0, 0, 0, 1 }, 2)]
+    [InlineData(new int[] { 1, 0, 0, 0, 0, 0, 1 }, 3)]
+    public void CanNotViolating(int[] flowers, int n)
+    {
+        var result = _worker.CanPlaceFlowers(flowers, n);
+
+        Assert.False(result);
+    }
+}
---
+++ TestMindBox/TestMindBox/LeetCode/CanPlaceFlowersTask.cs
@@ -0,0 +1,27 @@
+﻿namespace TestMindBox.LeetCode;
+
+/// <summary>
+/// https://leetcode.com/problems/can-place-flowers/description/?envType=study-plan-v2&envId=leetcode-75
+/// </summary>
+public class CanPlaceFlowersTask
+{
+    public bool CanPlaceFlowers(int[] flowerbed, int n)
+    {
+        for(int i = 0; i < flowerbed.Length; i++) 
+        {
+            var isPrev = (i == 0 || flowerbed[i - 1] == 0) && flowerbed[i] != 1;
+            var isNext = (i + 1 == flowerbed.Length || flowerbed[i + 1] == 0) && flowerbed[i] != 1;
+
+            if (isPrev && isNext)
+            {
+                flowerbed[i] = 1;
+                n--;
+            }
+
+            if (n <= 0)
+                return true;
+        }
+
+        return false;
+    }
+}
