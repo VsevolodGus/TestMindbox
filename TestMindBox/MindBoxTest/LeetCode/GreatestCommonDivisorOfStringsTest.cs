a2b2ef6f9838e90c7d56aa176b06e8975a8f56d8
user <user@WIN-2A6KSVNFBG3>
2023-07-18 03:43:06 +0500

    add task from LeetCode

---
+++ TestMindBox/MindBoxTest/LeetCode/GreatestCommonDivisorOfStringsTest.cs
@@ -0,0 +1,30 @@
+﻿using TestMindBox.LeetCode;
+
+namespace MindBoxTest.LeetCode;
+public class GreatestCommonDivisorOfStringsTest
+{
+    private readonly GreatestCommonDivisorOfStrings _worker = new();
+
+    [Theory]
+    [InlineData("LEET", "CODE")]
+    [InlineData("123", "321")]
+    [InlineData("ABCDEF", "ABC")]
+    [InlineData("ASDFGHJK", "ZXCVBNMQWERTYU")]
+    public void WithEmptyResult(string str1, string str2)
+    {
+        var result = _worker.GcdOfStrings(str1, str2);
+
+        Assert.Equal(string.Empty, result);
+    }
+
+    [Theory]
+    [InlineData("ABCABC", "ABC", "ABC")]
+    [InlineData("ABABAB", "ABAB", "AB")]
+    [InlineData("QWEQWEQWE", "QWEQWE", "QWE")]
+    public void HasCommonDivision(string str1, string str2, string exepcedResult)
+    {
+        var result = _worker.GcdOfStrings(str1, str2);
+
+        Assert.Equal(exepcedResult, result);
+    }
+}
---
+++ TestMindBox/TestMindBox/LeetCode/GreatestCommonDivisorOfStrings.cs
@@ -0,0 +1,32 @@
+﻿namespace TestMindBox.LeetCode;
+/// <summary>
+/// https://leetcode.com/problems/greatest-common-divisor-of-strings/description/?envType=study-plan-v2&envId=leetcode-75
+/// </summary>
+public class GreatestCommonDivisorOfStrings
+{
+    public string GcdOfStrings(string str1, string str2)
+    {
+        if (str1 + str2 != str2 + str1)
+            return string.Empty;
+
+        var minLenght = Math.Min(str1.Length, str2.Length);
+        for (int i = minLenght; i >= 0; i--)
+        {
+            if (IsValid(str1, str2, i))
+                return str1[..i];
+        }
+
+        return string.Empty;
+    }
+
+    public bool IsValid(string str1, string str2, int index)
+    {
+        if(str1.Length % index != 0 && str2.Length % index != 0)
+            return false;
+
+        var substring = str1[..index];
+        
+        return str1.Replace(substring, string.Empty) == string.Empty
+                && str2.Replace(substring, string.Empty) == string.Empty;
+    }
+}
