960373d1353652708041bae6c079d989bc9760cf
user <user@WIN-2A6KSVNFBG3>
2023-07-20 02:00:58 +0500

    add task from LeetCode

---
+++ TestMindBox/MindBoxTest/LeetCode/FindPeakElementTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestMindBox.LeetCode;
+
+namespace MindBoxTest.LeetCode;
+public class FindPeakElementTest
+{
+    private readonly FindPeakElementTask _worker = new();
+
+    [Theory]
+    [InlineData(new int[] { 7, 8, 9 })]
+    [InlineData(new int[] { 1, 3, 5, 7, 9 })]
+    [InlineData(new int[] { 0, 2, 4, 6, 8 })]
+    public void IfOrderArray(int[] nums)
+    {
+        var result = _worker.FindPeakElement(nums);
+
+        Assert.Equal(nums.Length - 1, result);
+    }
+
+
+    [Theory]
+    [InlineData(new int[] { 9, 8, 7 })]
+    [InlineData(new int[] { 9, 7, 5, 3, 1 })]
+    [InlineData(new int[] { 8, 6, 4, 2, 0 })]
+    public void IfOrderDecArray(int[] nums)
+    {
+        var result = _worker.FindPeakElement(nums);
+
+        Assert.Equal(0, result);
+    }
+
+    [Theory]
+    [InlineData(new int[] { 1, 2, 3, 1 }, 2)]
+    [InlineData(new int[] { 1, 2, 1, 3, 5, 6, 4 }, 5)]
+    public void MixedArray(int[] nums, int exceptedResult)
+    {
+        var result = _worker.FindPeakElement(nums);
+
+        Assert.Equal(exceptedResult, result);
+    }
+
+    [Theory]
+    [InlineData(new int[] { 1 })]
+    [InlineData(new int[] { 2 })]
+    public void ArrayOfOneElement(int[] nums)
+    {
+        var result = _worker.FindPeakElement(nums);
+
+        Assert.Equal(0, result);
+    }
+}
---
+++ TestMindBox/TestMindBox/LeetCode/FindPeakElementTask.cs
@@ -0,0 +1,25 @@
+﻿namespace TestMindBox.LeetCode;
+
+/// <summary>
+/// https://leetcode.com/problems/find-peak-element/?envType=study-plan-v2&envId=leetcode-75
+/// </summary>
+public class FindPeakElementTask
+{
+    public int FindPeakElement(int[] nums)
+    {
+        var left = 0;
+        var right = nums.Length - 1;
+
+        while (left + 1 < right)
+        {
+            var mid = left + (right - left) / 2;
+
+            if (nums[mid] < nums[mid + 1])
+                left = mid;
+            else
+                right = mid;
+        }
+
+        return nums[left] > nums[right] ? left : right;
+    }
+}
