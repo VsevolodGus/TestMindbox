cb04d031c79f9446b88e3ea3e5c88432c6db5ada
user <user@WIN-2A6KSVNFBG3>
2023-07-19 23:32:21 +0500

    add task from LeetCode

---
+++ TestMindBox/TestMindBox/LeetCode/SingleNumberTask.cs
@@ -0,0 +1,19 @@
+﻿namespace TestMindBox.LeetCode;
+
+/// <summary>
+/// https://leetcode.com/problems/single-number/description/?envType=study-plan-v2&envId=leetcode-75
+/// </summary>
+public class SingleNumberTask
+{
+    public int SingleNumber(int[] nums)
+    {
+        var result = 0;
+        foreach (var x in nums) 
+        {
+            result ^= x;
+        }
+
+        return result;
+    }
+}
+
---
+++ TestMindBox/TestMindBox/LeetCode/StringCompression.cs
@@ -0,0 +1,27 @@
+﻿namespace TestMindBox.LeetCode;
+public class StringCompression
+{
+    public int Compress(char[] chars)
+    {
+        var result = new List<char>();
+
+        int countSymbol = 0;
+        for (int i = 0; i < chars.Length; i++)
+        {
+            countSymbol++;
+
+            if (countSymbol == 9
+                || chars[i] != chars[i+1])
+            {
+                result.Add(chars[i]);
+                if(countSymbol != 1)
+                    result.Add((char)('0' + countSymbol));
+
+                countSymbol = 0;
+            }
+        }
+
+        chars = result.ToArray();
+        return result.Count;
+    }
+}
