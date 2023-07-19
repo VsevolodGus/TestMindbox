2d80168eda00df04413e426436c9b840e7a8c001
user <user@WIN-2A6KSVNFBG3>
2023-07-20 03:10:26 +0500

    add task from LeetCode

---
+++ TestMindBox/MindBoxTest/LeetCode/MaximumNumberOfVowelsInSubstringOfGivenLengthTest.cs
@@ -0,0 +1,38 @@
+﻿using TestMindBox.LeetCode;
+
+namespace MindBoxTest.LeetCode;
+public class MaximumNumberOfVowelsInSubstringOfGivenLengthTest
+{
+    private readonly MaximumNumberOfVowelsInSubstringOfGivenLength _worker = new();
+
+    [Theory]
+    [InlineData("aeiou", 2)]
+    public void IfFullVolwes(string s, int k)
+    {
+        var result = _worker.MaxVowels(s, k);
+
+        Assert.Equal(2, result);
+    }
+
+    [Theory]
+    [InlineData("qwrtpsdfghjklxcvbnm", 2)]
+    public void IfFullNotVolwes(string s, int k)
+    {
+        var result = _worker.MaxVowels(s, k);
+
+        Assert.Equal(0, result);
+    }
+
+    [Theory]
+    [InlineData("abciiidef", 3, 3)]
+    [InlineData("leetcode", 3, 2)]
+    [InlineData("weallloveyou", 7, 4)]
+    [InlineData("rhythms", 0, 0)]
+    [InlineData("tryhard", 4, 1)]
+    public void MixString(string s, int k, int exceptedResult)
+    {
+        var result = _worker.MaxVowels(s, k);
+
+        Assert.Equal(exceptedResult, result);
+    }
+}
---
+++ TestMindBox/TestMindBox/LeetCode/MaximumNumberOfVowelsInSubstringOfGivenLength.cs
@@ -0,0 +1,30 @@
+﻿namespace TestMindBox.LeetCode;
+
+/// <summary>
+/// https://leetcode.com/problems/maximum-number-of-vowels-in-a-substring-of-given-length/?envType=study-plan-v2&envId=leetcode-75
+/// </summary>
+public class MaximumNumberOfVowelsInSubstringOfGivenLength
+{
+    private readonly HashSet<char>_vowels = new(new char[] { 'a', 'e', 'i', 'o', 'u' });
+
+    public int MaxVowels(string s, int k)
+    {
+        if (k == 0)
+            return 0;
+
+        var count = s[..k].Count(_vowels.Contains);
+        var maxCountVowels = count;
+        for (int i = k; i < s.Length; i++)
+        {
+            var asd = s.Substring(i - k, k);
+
+            count -= _vowels.Contains(s[i - k]) ? 1 : 0;
+            count += _vowels.Contains(s[i]) ? 1 : 0;
+
+            if (count > maxCountVowels)
+                maxCountVowels = count;
+        }
+
+        return maxCountVowels;
+    }
+}
