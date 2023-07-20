55ed5cd66360f07a14213b8436e321d69ac85d1b
user <user@WIN-2A6KSVNFBG3>
2023-07-21 02:07:18 +0500

    add task from LeetCode

---
+++ TestMindBox/MindBoxTest/LeetCode/TribonacciNumberTest.cs
@@ -0,0 +1,43 @@
+﻿using TestMindBox.LeetCode;
+
+namespace MindBoxTest.LeetCode;
+public class TribonacciNumberTest
+{
+    private readonly TribonacciNumber _worker = new();
+
+    /// <summary>
+    /// n > 2
+    /// </summary>
+    /// <param name="n"></param>
+    /// <param name="exceptedResult"></param>
+    [Theory]
+    [InlineData(3, 2)]
+    [InlineData(4, 4)]
+    [InlineData(7, 24)]
+    [InlineData(25, 1389537)]
+    public void NMore2(int n, int exceptedResult)
+    {
+        var result = _worker.Tribonacci(n);
+
+        Assert.Equal(exceptedResult, result);
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    public void N1Or2(int n)
+    {
+        var result = _worker.Tribonacci(n);
+
+        Assert.Equal(1, result);
+    }
+
+    [Theory]
+    [InlineData(0)]
+    public void NZero(int n)
+    {
+        var result = _worker.Tribonacci(n);
+
+        Assert.Equal(0, result);
+    }
+}
---
+++ TestMindBox/TestMindBox/LeetCode/TribonacciNumber.cs
@@ -0,0 +1,20 @@
+﻿namespace TestMindBox.LeetCode;
+public class TribonacciNumber
+{
+    public int Tribonacci(int n)
+    {
+        var list = new List<int>(n)
+        {
+            0, 1, 1
+        };
+        
+        if(n <= 2)
+            return list[n];
+
+        for (int i = 3; i <= n; i++)
+            list.Add(list[i-1]+ list[i - 2]+ list[i - 3]);
+        
+
+        return list.Last();
+    }
+}
