0de9b46ea54e845f748aadc3fcdd9a60de6f34a6
user <user@WIN-2A6KSVNFBG3>
2023-07-22 01:14:03 +0500

    ad task from LeetCode

---
+++ TestMindBox/MindBoxTest/LeetCode/MaxNumberOfKSumPairsTest.cs
@@ -0,0 +1,17 @@
+﻿using TestMindBox.LeetCode;
+
+namespace MindBoxTest.LeetCode;
+public class MaxNumberOfKSumPairsTest
+{
+    private readonly MaxNumberOfKSumPairs _worker = new();
+
+    [Theory]
+    [InlineData(new int[] { 1, 2, 3, 4 }, 5, 2)]
+    [InlineData(new int[] { 3, 1, 3, 4, 3 }, 6, 1)]
+    public void Test(int[] nums, int k, int exceptedResult)
+    {
+        var result = _worker.MaxOperations(nums, k);
+
+        Assert.Equal(exceptedResult, result);
+    }
+}
---
+++ TestMindBox/TestMindBox/LeetCode/MaxNumberOfKSumPairs.cs
@@ -0,0 +1,31 @@
+﻿namespace TestMindBox.LeetCode;
+public class MaxNumberOfKSumPairs
+{
+    public int MaxOperations(int[] nums, int k)
+    {
+        Array.Sort(nums);
+        var result = 0;
+
+        int leftIndex = 0;
+        int rightIndex = nums.Length - 1;
+
+        while (leftIndex < rightIndex)
+        {
+            int sum = nums[leftIndex] + nums[rightIndex];
+
+            if (sum == k)
+            {
+                result++;
+                rightIndex--;
+                leftIndex++;
+            }
+            else if(sum < k)
+                leftIndex++;
+            else
+                rightIndex--;
+        }
+
+
+        return result;
+    }
+}
